0adae4b4181a52a465649a36e23824b044932277
gongbudaizhe <gongbudaizhe@gmail.com>
2014-11-18 20:04:15 +0800

    file2utf8.py: cosmetic modification and bug fixes

--- utils/file2utf8.py
+++ utils/file2utf8.py
@@ -17,29 +17,35 @@ TODO:
     2. convert_tree_to_utf8's handling directory name doesn't work correctlly
     3. convert_tree_to_utf8 handle suffix
     4. argparse add more options
-    DONE! for large files, it can consume a lot of memory(for 2.5G file, you need a machine with memory larger than 6G), I shall figure out a way to solve this.
+    DONE! for large files, it can consume a lot of memory(for 2.5G file,
+            you need a machine with memory larger than 6G),
+            I shall figure out a way to solve this.
 
 """
 
 import sys
 import os
 
+
 def readline(file_, num, lines):
     for i in range(num):
         lines.append(file_.readline())
-    
+
     return len(lines[-1]) > 0
- 
+
+
 def mkdir_p(path):
     """functions like mkdir -p in bash"""
 
     import errno
     try:
         os.makedirs(path)
-    except OSError as exc: # Python > 2.5
+    except OSError as exc:  # Python > 2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        else: raise
+        else:
+            raise
+
 
 def get_available_encodings():
     """get all available encodings in python"""
@@ -49,19 +55,21 @@ def get_available_encodings():
 
     false_positives = set(["aliases"])
 
-    found = set(name for imp, name, ispkg in pkgutil.iter_modules(encodings.__path__) if not ispkg)
+    found = set(name for imp, name, ispkg in
+                pkgutil.iter_modules(encodings.__path__) if not ispkg)
     found.difference_update(false_positives)
 
     return found
-    
-#available_encodings = get_available_encodings()
 
-def convert_tree_to_utf8(dirname, autodetect = True, first_try_cp = None):
+# available_encodings = get_available_encodings()
+
+
+def convert_tree_to_utf8(dirname, autodetect=True, first_try_cp=None):
     """convert a whole tree to utf8"""
 
     new_top_dir = dirname + ".utf8"
     mkdir_p(new_top_dir)
-    for dirpath, dirnames,filenames in os.walk(dirname):
+    for dirpath, dirnames, filenames in os.walk(dirname):
         new_dirpath = new_top_dir + "/" + '/'.join(dirpath.split('/')[1:])
         for dir_ in dirnames:
             mkdir_p(os.path.join(new_dirpath, dir_))
@@ -70,12 +78,16 @@ def convert_tree_to_utf8(dirname, autodetect = True, first_try_cp = None):
             new_file = os.path.join(new_dirpath, name)
             with open(old_file) as f_in:
                 with open(new_file, "w") as f_out:
-                    f_out.write(convert_to_utf8(f_in.read(), autodetect, first_try_cp))
+                    f_out.write(convert_to_utf8(f_in.read(),
+                                                autodetect,
+                                                first_try_cp))
 
-def find_encoding(string, autodetect = True, first_try_cp = None):
+
+def find_encoding(string, autodetect=True, first_try_cp=None):
     """find encodings for any string"""
 
-    string_partial = string[:8000] # we don't need all of the string to find the encoding
+    string_partial = string[:8000]  # we don't need all of the string to find
+                                    # the encoding
     code_page_found = False
     encoding = None
     if first_try_cp:
@@ -87,7 +99,7 @@ def find_encoding(string, autodetect = True, first_try_cp = None):
             pass
 
     if not code_page_found and autodetect:
-        import chardet    
+        import chardet
         detected_encoding = (chardet.detect(string[:8000]))['encoding']
         try:
             string_decode = string_partial.decode(detected_encoding)
@@ -97,26 +109,26 @@ def find_encoding(string, autodetect = True, first_try_cp = None):
             pass
 
     if not code_page_found:
-        source_encoding = ["gbk" 
-                           ,"gb2312" 
-                           ,"gb18030" 
-                           ,"big5" 
-                           ,"shift_jis" 
-                           ,"iso2022_kr" 
-                           ,"iso2022_jp" 
-                           ,"ascii"] 
+        source_encoding = ["gbk",
+                           "gb2312",
+                           "gb18030",
+                           "big5",
+                           "shift_jis",
+                           "iso2022_kr",
+                           "iso2022_jp",
+                           "ascii"]
         for x in source_encoding:
             try:
-                string_decode = string_partial.decode(x)  
+                string_decode = string_partial.decode(x)
                 code_page_found = True
                 encoding = x
             except UnicodeDecodeError:
-                continue 
+                continue
 
     return encoding
 
 
-def convert_to_utf8(string, autodetect = True, first_try_cp = None):
+def convert_to_utf8(string, autodetect=True, first_try_cp=None):
     """convert string from all kinds of encoding to UTF-8"""
 
     code_page_found = False
@@ -128,7 +140,7 @@ def convert_to_utf8(string, autodetect = True, first_try_cp = None):
             pass
 
     if autodetect:
-        import chardet    
+        import chardet
         detected_encoding = (chardet.detect(string[:8000]))['encoding']
         try:
             string_decode = string.decode(detected_encoding[:5000])
@@ -137,19 +149,19 @@ def convert_to_utf8(string, autodetect = True, first_try_cp = None):
             pass
 
     if not code_page_found:
-        source_encoding = ["gbk" 
-                           ,"gb2312" 
-                           ,"gb18030" 
-                           ,"big5" 
-                           ,"shift_jis" 
-                           ,"iso2022_kr" 
-                           ,"iso2022_jp" 
-                           ,"ascii"] 
+        source_encoding = ["gbk",
+                           "gb2312",
+                           "gb18030",
+                           "big5",
+                           "shift_jis",
+                           "iso2022_kr",
+                           "iso2022_jp",
+                           "ascii"]
         for encoding in source_encoding:
             try:
-                string_decode = string.decode(encoding)  
+                string_decode = string.decode(encoding)
             except UnicodeDecodeError:
-                continue 
+                continue
 
     string_encode = string_decode.encode('utf-8')
     return string_encode
@@ -157,7 +169,7 @@ def convert_to_utf8(string, autodetect = True, first_try_cp = None):
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help = 'specify input file')
+    parser.add_argument('filename', help='specify input file')
     args = parser.parse_args()
 
     filename = args.filename
@@ -181,14 +193,19 @@ if __name__ == '__main__':
             lines = []
             status = readline(input_file, 100, lines)
 
+            errors = 0
             for line in lines:
                 if len(line) > 0:
-                    line = line.decode(encoding)
-                    new_line = line.encode("utf-8")
-                    sys.stdout.write(new_line)
+                    try:
+                        line = line.decode(encoding)
+                        new_line = line.encode("utf-8")
+                        sys.stdout.write(new_line)
+                    except:
+                        errors += 1
 
             # remember to jump out the while loop
             if not status:
+                sys.stderr.write("number of errors: " + str(errors) + "\n")
                 break
     else:
         sys.stderr.write("Sorry, I can't find the encoding of input file...")
