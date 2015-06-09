7c69beaa6aa05e8d65ad9cc2c1e6054c40845b29
gongbudaizhe <gongbudaizhe@gmail.com>
2015-06-09 09:52:00 +0800

    reduce repeated computation

--- demos/square_supplies/solution.py
+++ demos/square_supplies/solution.py
@@ -1,9 +1,9 @@
 def answer(x):
     x_sqrt = x**0.5
-    if x_sqrt == int(x_sqrt):
+    x_sqrt_int = int(x_sqrt)
+    if x_sqrt == x_sqrt_int:
         return 1
     else:
-        x_sqrt_int = int(x_sqrt)
         current_best = 1000000
         for i in xrange(x_sqrt_int, 0, -1):
             res = x - x_sqrt_int**2
